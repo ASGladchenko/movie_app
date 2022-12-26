d095d3685707ef48923b9cc581f20255ad02d543
ASGladchenko <asgladchenko@gmail.com>
2022-12-26 14:53:47 +0200

    added test-version of project

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
 
 import App from './App';
+
+import store from "./store/configureStore";
 
 import reportWebVitals from './reportWebVitals';
 
 import './index.css';
 
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
     <BrowserRouter>
-        <App />
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </BrowserRouter>
-
 );
 
 // If you want to start measuring performance in your app, pass a function
